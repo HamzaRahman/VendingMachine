5c0eb0a36bf0d59322e8396df6df82a64626c6b4
Hamza <hamza.rahman2016@outlook.com>
2021-07-08 22:17:10 +0500

    (VendingMachine) Added a Dictionary of 3 Types of Products and Total of 8 Products

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -6,5 +6,37 @@ namespace VendingMachine
 {
     public class VendingMachine : IVending
     {
+        Dictionary<string, Product> Products = new Dictionary<string, Product>()
+        {{"P1",new Drink("CocaCola",5)},
+         {"P2",new Drink("Pepsi",5)},
+         {"P3",new Drink("Mirinda",5)},
+         {"P4",new Drink("Sprite",5)},
+
+         {"P5",new Food("Chocolate",5.4M)},
+         {"P6",new Food("Chips",3.77M)},
+
+         {"P7",new Toy("Car",5.4M)},
+         {"P8",new Toy("Doll",5.4M)},
+        };
+
+        public Dictionary<string, decimal> EndTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal InsertMoney(int coin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Purchase(string ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
