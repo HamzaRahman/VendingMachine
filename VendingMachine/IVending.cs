a4cd1f33cd7177710d778a1d6d9edf5428f229f2
Hamza <hamza.rahman2016@outlook.com>
2021-07-08 22:05:03 +0500

    Added IVending Interface

---
+++ VendingMachine/IVending.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    public interface IVending
+    {
+        decimal Purchase(string ID);
+        void ShowAll();
+        decimal InsertMoney(int coin);
+        Dictionary<string, decimal> EndTransaction();
+    }
+}
